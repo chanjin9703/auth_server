0b8afabd781bb61b08a5c18bb5748726400bb986
chanjin9703 <155693092+chanjin9703@users.noreply.github.com>
2024-01-31 14:20:10 +0900

    Update cognito.js

--- cognito.js
+++ cognito.js
@@ -6,7 +6,9 @@ const request = require("request"); //http요청
 //jwt 사용 패키지
 const jwkToPem = require("jwk-to-pem"); 
 const jwt = require("jsonwebtoken");
+const axios = require('axios');
 
+const cognitoTokenEndpoint = 'https://auth-server.auth.us-east-1.amazoncognito.com/oauth2/token';
 
 const poolData = {
   UserPoolId: "us-east-1_aSlGxRnxZ",
@@ -16,6 +18,7 @@ const poolData = {
 const aws_region = "us-east-1";
 
 const CognitoUserPool = AmazonCognitoId.CognitoUserPool;
+var CognitoRefreshToken = AmazonCognitoId.CognitoRefreshToken;
 const userPool = new AmazonCognitoId.CognitoUserPool(poolData);
 
 //cognito 회원가입
@@ -31,9 +34,9 @@ const signUp = (email, password) => {
                     Name: "email",
                     Value: email
                 })
-            );          
-             
-        
+            );
+          
+
 
         //cognito에 새 user 등록.
         userPool.signUp(email, password, attributeList, null, (err, data) => {
@@ -72,11 +75,12 @@ const verifyCode = (username, code) => {
 const logIn = (name, password) => {
     return new Promise((resolve, reject) => {
         try {
+            //사용자 인증정보생성
             const authenticationDetails = new AmazonCognitoId.AuthenticationDetails({
                 Username: name,
                 Password: password
             });
-
+            //사용자 데이터설정
             const userData = {
                 Username: name,
                 Pool: userPool
@@ -84,12 +88,14 @@ const logIn = (name, password) => {
 
             const cognitoUser = new AmazonCognitoId.CognitoUser(userData);
 
+            //사용자인증
             cognitoUser.authenticateUser(authenticationDetails, {
                 onSuccess: result => {
                     resolve({
                         accesToken: result.getAccessToken().getJwtToken(),
                         idToken: result.getIdToken().getJwtToken(),
-                        refreshToken: result.getRefreshToken().getToken()
+                        refreshToken: result.getRefreshToken().getToken(),
+                        username: name
                     });
                 },
                 onFailure: err => {
@@ -101,6 +107,59 @@ const logIn = (name, password) => {
         }
     });
 };
+
+//로그아웃
+const logout = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            const cognitoUser = userPool.getCurrentUser();
+
+            if (cognitoUser) {
+                cognitoUser.signOut();
+                resolve("Logout successful");
+            } else {
+                reject("No user is currently signed in");
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+
+
+//토큰갱신
+async function refreshTokens(username,refreshToken) {
+    try {
+        const userData = {
+            Username: username,
+            Pool: userPool
+        };
+
+        const cognitoUser = new AmazonCognitoId.CognitoUser(userData);
+        
+        const token = new CognitoRefreshToken({RefreshToken: refreshToken});
+        
+        const session = await new Promise((resolve, reject) => {
+            cognitoUser.refreshSession(token, function (err, session) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(session);
+                }
+            });
+        });
+        const idToken = session.getIdToken().getJwtToken();
+        const accessToken = session.getAccessToken().getJwtToken();
+
+        return { idToken, accessToken};
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
 
 // 비밀번호 변경
 const changePwd = (username, password, newpassword)=> {
@@ -131,6 +190,8 @@ const changePwd = (username, password, newpassword)=> {
     });
 };
 
+module.exports.logout = logout;
+module.exports.refreshTokens = refreshTokens;
 module.exports.changePwd = changePwd;
 module.exports.logIn = logIn;
 module.exports.verifyCode = verifyCode;
