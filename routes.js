f02b6387a80cce2c2b6dca8d0fd1ef4dd964024b
chanjin9703 <155693092+chanjin9703@users.noreply.github.com>
2024-01-31 14:20:34 +0900

    Update routes.js

--- routes.js
+++ routes.js
@@ -8,10 +8,12 @@ router.post('/signup', async (req,res)=> {
     const {body} = req;
 
     //필요한 포멧 정의.
+    //if (body.email&&body.user&&body.password){
     let {email,password} = body;
 
     try {
 
+      
       //cognito 모듈사용, 회원가입처리
       let result = await cognito.signUp(email,password);
       //성공시 응답반환
@@ -28,7 +30,10 @@ router.post('/signup', async (req,res)=> {
 
       res.status(400).json({"error":err});
     }              
-                
+/*
+    } else {
+        res.status(400).json({"error": "bad format"});
+    }*/                
 });
 
 //이메일인증
@@ -75,8 +80,39 @@ router.post('/login', async (req,res)=> {
   }
 });
 
+//로그아웃
+router.post('/logout', async (req, res) => {
+  try {
+    await cognito.logout();
+    res.status(200).json({ message: "Logout successful"});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message});
+  }
+});
+
+//refresh token 사용해서 access,id token 갱신
+router.post('/refresh', async (req, res) => {
+  const { body } = req;
+  if (body.refresh_token) {
+    const { refresh_token, username } = body;
+    
+   
+    try {
+        let result = await cognito.refreshTokens(username,refresh_token);
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(response);
+        res.status(400).json({ "error": error});
+    }
+  } else {
+      
+      res.status(400).json({ "error": "refreshToken is required"});
+  }
+});
+
 //비밀번호변경
-router.post('/changePwd', async (req,res)=> {
+router.post('/changepwd', async (req,res)=> {
 
   const {body} = req;
 
